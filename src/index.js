8f70887304b1dfaef2349aad9fb048cc5273fb8f
joncasey <joncasey@gmail.com>
2019-11-13 18:09:40 -0500

    Add `save()` to simplify saving to local directory Note: this will remove target directory & save fresh copy

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import getFont from './get-font'
 import downloadFont from './download-font'
+import { rmdirSync } from 'fs'
 
 // expose for debug outside of this lib
 import * as parse from './parse'
 import * as util from './util'
 
+async function save (fontFamily, toFolder) {
+  const Font = await getFont(fontFamily)
+  rmdirSync(toFolder, { recursive: true })
+  const result = await downloadFont(Font, toFolder)
+  return { ...Font, result }
+}
+
 export {
   getFont,
   downloadFont,
   parse,
+  save,
   util
 }
 
