231d7fb45d7287d3cfb992cb6cbcdfbc2ebd8c4e
joncasey <joncasey@gmail.com>
2019-11-13 18:04:20 -0500

    Improve index.html syntax  - no-inline-style warnings  - show "normal" font-style first

--- src/create-index-html.js
+++ src/create-index-html.js
@@ -1,21 +1,31 @@
 
-export default (fonts = [{}], family = fonts[0].family) => {
+const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const lower = 'abcdefghijklmnopqrstuvwxyz'
+const extra = '0123456789,.!?'
 
-  const paragraphs = fonts.map(font =>
-`  <p style="font-family:'${family}',monospace;font-weight:${font.weight};font-style:${font.style};">ABCDEFGHIJKLMNOPQRSTUVWXYZ<br>abcdefghijklmnopqrstuvwxyz<br>0123456789,.!?</p>`
-  )
+const fontWeights = '12345678'.split('').map(v => `
+    .fw${v}00 { font-weight: ${v}00; }`)
 
-  return `
-<!DOCTYPE html>
+const fontToParagraph = font => `
+  <p class="fw${font.weight}${/i/.test(font.style) ? ' i' : '' }">${upper}<br>${lower}<br>${extra}</p>`
+
+const sortNormalFirst = ({ style: a }, { style: b }) => a > b ? -1 : a < b ? 1 : 0
+
+export default (fonts = [{}], family = fonts[0].family) => `<!DOCTYPE html>
 <html lang="en">
 <head>
   <title>${family}</title>
   <link rel="stylesheet" href="index.css">
+  <style>
+    body { font-family: '${family}', monospace; font-size: 100%; }
+    p { line-height: 1.6; }
+    .i { font-style: italic; }${
+    fontWeights.join('')}
+  </style>
 </head>
 <body>
-${paragraphs.join('\n')}
+  <h1>${family}</h1>${
+  fonts.sort(sortNormalFirst).map(fontToParagraph).join('')}
 </body>
 </html>
-  `.trim()
-
-}
+`
